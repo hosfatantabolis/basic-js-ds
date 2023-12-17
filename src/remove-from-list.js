ed4700387d33bd451c608ef25189b7e1643449e1
Maksim Litvinov <lmk478shade@gmail.com>
2023-12-17 23:09:51 +0300

    feat: remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  while (l !== null && l.value === k){
+    l = l.next;
+  }
+  let prev = l;
+  let curr = l.next;
+  while(curr !== null){
+    if(curr.value === k){
+      prev.next = curr.next;
+    } else {
+      prev = curr;
+    }
+    curr = curr.next;
+  }
+  return l;
 }
 
 module.exports = {
